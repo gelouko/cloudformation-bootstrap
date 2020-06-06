f0e0cb09edc588bc740ee503df14847d36e1c9dd
Giulio Denardi <giulio.lpdenardi@gmail.com>
2020-06-06 01:07:13 -0300

    feat(providers): add integration for the terraform provider

--- {{cookiecutter.project_slug}}/README.md
+++ {{cookiecutter.project_slug}}/README.md
@@ -1,5 +1,13 @@
 # {{cookiecutter.project_name}}
 
+## Requirements
+- [pipenv](https://pypi.org/project/pipenv/)
+- [terraform](https://www.terraform.io/downloads.html)
+- [terragrunt](https://terragrunt.gruntwork.io/docs/getting-started/install/)
+- [terraform-docs](https://github.com/segmentio/terraform-docs)
+- [tflint](https://github.com/terraform-linters/tflint)
+- [tfsec](https://github.com/liamg/tfsec)
+
 ## Setup
 
 Just run `pipenv run setup` to setup your repository and start creating your templates!
---
+++ {{cookiecutter.project_slug}}/terraform/s3-bucket/s3-bucket.tf
@@ -0,0 +1,29 @@
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "Dev"
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.my_bucket.id
+    target_prefix = "log/"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
